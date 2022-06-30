076622ff5b9fc16e025176d3ade7de61fae56162
Keenan Faulkner <keenan.r.faulkner@gmail.com>
2022-06-29 23:08:02 -0600

    Added admin login and view play count functionality. Ready for deploy

--- server/controller.js
+++ server/controller.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const bcrypt = require('bcryptjs');
 const {CONNECTION_STRING} = process.env;
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(CONNECTION_STRING, {
@@ -27,13 +28,13 @@ module.exports = {
         .catch(err => console.log(err));
     },
 
-    getCover: (req, res) => {
+    getCoverAudio: (req, res) => {
         const { id } = req.params;
         sequelize.query(`
-            SELECT url FROM covers
+            SELECT audio_url FROM covers
             WHERE ${id} = cover_id`)
         .then(dbRes => {
-            res.status(200).send(dbRes[0][0].url);
+            res.status(200).send(dbRes[0][0].audio_url);
         })
         .catch(err => console.log(err));
     },
@@ -46,5 +47,43 @@ module.exports = {
             WHERE ${id} = cover_id`)
         .then(res.status(200).send('Thanks for listening!'))
         .catch(err => console.log(err));
-    }
+    },
+
+    getKey: (req, res) => {
+        const receivedPassword = req.body.password;
+        const userID = req.body.username;
+        let storedPassword;
+        sequelize.query(`
+            SELECT password FROM passwords
+            WHERE user_name = '${userID}'`)
+        .then(dbRes => {
+            if(typeof dbRes[0][0] === 'undefined') {
+                res.status(401).send('There was a problem authenticating your request');
+                return;
+            }
+            storedPassword = dbRes[0][0].password;
+            let matching = bcrypt.compareSync(receivedPassword, storedPassword);
+            if(!matching) {
+                res.status(401).send('There was a problem authenticating your request');
+            } else {
+                sequelize.query(`
+                    SELECT access_key FROM connection_data
+                    WHERE key_id = 1`)
+                .then(dbRes => {
+                    res.status(200).send(dbRes[0][0].access_key);
+                })
+                .catch(err => console.log(err));
+            }
+        })
+        .catch(err => console.log(err));
+    },
+
+    getAllCovers: (req, res) => {
+        sequelize.query(`
+            SELECT * FROM covers`)
+        .then(dbRes => {
+            res.status(200).send(dbRes[0]);
+        })
+        .catch(err => console.log(err));
+    },
 }
